dc06f95fb6a185c0c20a4d3295e52ad89dfd7969
Shahid Nawaz <shahid_nawaz_intern@netixsol.com>
2023-12-16 16:31:34 -0800

    frontend almost completed

--- expanse_tracker/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Header } from "./Header.jsx";
-export { default as Balance } from "./Balance.jsx";
-export { default as IncomeExpensess } from "./IncomeExpensess.jsx";
-export { default as TransactionsList } from "./TransactionsList.jsx";
-export { default as AddTransaction } from "./AddTransaction.jsx";
