3fb357ea5f8de84239cb293e5c03f9ef6bc5f7f7
Shahid Nawaz <shahid_nawaz_intern@netixsol.com>
2023-12-18 22:15:15 -0800

    ..

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-
 const { db } = require("./db/db");
 const { readdirSync } = require("fs");
-//const { route } = require("./routes/transactions");
 const app = express();
 
 require("dotenv").config();
@@ -14,11 +12,15 @@ app.use(cors());
 app.use(express.json());
 
 // routes
-readdirSync("./routes").map((route) =>
-  app.use("/api/v1", require(`./routes/${route}`))
-);
-const routes = readdirSync("./routes");
-console.log(routes);
+const routesDir = "./routes";
+try {
+  const routes = readdirSync(routesDir);
+  routes.map((route) => app.use("/api/v1", require(`${routesDir}/${route}`)));
+  console.log(routes);
+} catch (error) {
+  console.error("Error reading routes directory:", error);
+}
+
 const Server = () => {
   db();
   app.listen(PORT, () => {
---
+++ frontend/src/Store/ExpenseStore.js
@@ -0,0 +1,14 @@
+// import { create } from "zustand";
+// import {devtools,persist} from "zustand/middleware";
+
+
+// const ExpenseStore = (set) =>{
+
+// }
+
+// const useExpenseStore = create({
+//     devtools:{
+//         persist(transaction,{
+//             ta
+//         })
+// })
