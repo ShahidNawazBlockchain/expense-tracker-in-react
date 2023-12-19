c2080fe804fa3f8dc59baf8f8b51e1e869ac02eb
Shahid Nawaz <shahid_nawaz_intern@netixsol.com>
2023-12-18 23:10:37 -0800

    ..

--- backend/db/db.js
+++ backend/db/db.js
@@ -2,11 +2,14 @@ const mongoose = require("mongoose");
 
 const db = async () => {
   try {
-    mongoose.set("strictQuery", false);
-    await mongoose.connect(process.env.MONGOOSE_URL);
+    await mongoose.connect(process.env.MONGOOSE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("DB connected");
   } catch (error) {
     console.log("Db is not connected", error.message);
   }
 };
+
 module.exports = { db };
